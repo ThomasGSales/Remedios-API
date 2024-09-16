43d3f9effb65e07c6cc927a7aa80471295afed4d
thomasgsales <tgaabriel444@gmail.com>
2024-09-16 19:29:46 -0300

    RemedioRepository 1.0

---
+++ src/main/java/com/remedios/thomas/curso/remedio/RemedioRepository.java
@@ -0,0 +1,15 @@
+package com.remedios.thomas.curso.remedio;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemedioRepository extends JpaRepository<Remedio, Long>{
+
+	List<Remedio> findAllByAtivoTrue();
+
+	List<Remedio> findAllByAtivoFalse();
+	
+
+}
